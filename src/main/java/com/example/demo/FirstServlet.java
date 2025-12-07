917c3ac16347c4522faa2fdcc6cad6a70ada94ae
Reshad26 <aliev.resad1999@gmail.com>
2025-12-07 19:25:34 +0400

    My first commit

---
+++ src/main/java/com/example/demo/FirstServlet.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("first")
+public class FirstServlet extends HttpServlet {
+
+
+
+    @Override
+    public void init(ServletConfig config) throws ServletException{
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        try (var writer = resp.getWriter()) {
+            writer.write("Hello from first servlet");
+        }
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
---
+++ src/main/java/com/example/demo/Main.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import jakarta.servlet.http.HttpServlet;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello ");
+
+
+
+    }
+}
